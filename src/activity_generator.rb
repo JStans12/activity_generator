e1e22cf270c071642d5cf1978b537e2d02b61cc4
Joey Stansfield <joseph.r.stansfield@gmail.com>
2020-12-19 19:02:28 -0700

    generate network activity

--- src/activity_generator.rb
+++ src/activity_generator.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'socket'
 require 'sys/proctable'
 include Sys
 
@@ -9,6 +10,7 @@ class ActivityGenerator
   TASK_TYPE_CREATE_FILE = 'create_file'
   TASK_TYPE_MODIFY_FILE = 'modify_file'
   TASK_TYPE_REMOVE_FILE = 'remove_file'
+  TASK_TYPE_NETWORK_CONNECTION = 'network_connection'
   EXECUTE_CMDLINE = 'cmdline'
   PATH_TO_FILE = 'path_to_file'
   COMMAND_TOUCH = 'touch %{path_to_file}'
@@ -30,6 +32,8 @@ class ActivityGenerator
         execute_file_task(TASK_TYPE_MODIFY_FILE, COMMAND_ECHO, task[PATH_TO_FILE])
       when TASK_TYPE_REMOVE_FILE
         execute_file_task(TASK_TYPE_REMOVE_FILE, COMMAND_REMOVE, task[PATH_TO_FILE])
+      when TASK_TYPE_NETWORK_CONNECTION
+        open_network_connection
       else
         p "UNKNOWN TASK TYPE - #{task['type']}"
       end
@@ -46,10 +50,9 @@ class ActivityGenerator
 
   def execute_task(cmdline, context = {})
     pid = spawn(cmdline)
-    # We don't care about the termination status, so we detach to avoid zombie processes.
     log_task(pid, context)
+    # We don't care about the termination status, so we detach to avoid zombie processes.
     Process.detach(pid)
-
   end
 
   def execute_file_task(type, command, path_to_file)
@@ -59,6 +62,28 @@ class ActivityGenerator
       path_to_file: File.expand_path(path_to_file)
     }
     execute_task(cmdline, context)
+  end
+
+  def open_network_connection
+    socket = TCPSocket.new('google.com', 80)
+    bytes = socket.write("random message")
+
+    _, source_port, source_address, _ = socket.addr
+    _, destination_port, destination_address, _ = socket.peeraddr
+
+    context = {
+      activity_type: TASK_TYPE_NETWORK_CONNECTION,
+      source_address: source_address,
+      source_port: source_port,
+      destination_address: destination_address,
+      destination_port: destination_port,
+      bytes_sent: bytes,
+      protocol: "TCP/IP"
+    }
+
+    log_task(Process.pid, context)
+
+    socket.shutdown
   end
 
   def log_task(pid, context = {})

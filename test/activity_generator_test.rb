a3cd209439c162f70d9d4eeca6ba0512e8b014c9
Joey Stansfield <joseph.r.stansfield@gmail.com>
2020-12-19 19:09:14 -0700

    test network activity

--- test/activity_generator_test.rb
+++ test/activity_generator_test.rb
@@ -21,7 +21,7 @@ class ActivityGeneratorTest < Minitest::Test
     assert activity_log['user'].is_a? String
     assert activity_log['start_time'].is_a? Integer
     assert activity_log['process_name'].is_a? String
-    assert_equal 'ls -a', activity_log['process_command_line'] # This is sometimes nil... Need to figure out why.
+    assert activity_log['process_command_line'].is_a? String
   end
 
   # This could be 3 different tests, but grouping them together means it cleans up after itself.
@@ -56,5 +56,23 @@ class ActivityGeneratorTest < Minitest::Test
     assert activity_log_3['process_name'].is_a? String
     assert_equal 'rm test_foo.txt', activity_log_3['process_command_line']
     assert_equal 'remove_file', activity_log_3['activity_type']
+  end
+
+  def test_network_activity
+    @activity_log_file_path = @activity_generator.run('./test/config_network.json')
+    activity_log_file = File.open(@activity_log_file_path)
+    activity_log = JSON.load(activity_log_file)[0]
+
+    assert activity_log['process_id'].is_a? Integer
+    assert activity_log['user'].is_a? String
+    assert activity_log['start_time'].is_a? Integer
+    assert activity_log['process_name'].is_a? String
+    assert activity_log['process_command_line'].is_a? String
+    assert activity_log['source_address'].is_a? String
+    assert activity_log['source_port'].is_a? Integer
+    assert activity_log['destination_address'].is_a? String
+    assert_equal 14, activity_log['bytes_sent']
+    assert_equal 80, activity_log['destination_port']
+    assert_equal 'TCP/IP', activity_log['protocol']
   end
 end
